fb4ad455c9231f60e5fbaae191b9ff86a2c96e49
LEEMINWOO <ymca0626@naver.com>
2024-11-15 16:22:04 +0900

    help me plz

--- AI/myenv/Scripts/activate.bat
+++ AI/myenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sojun\Desktop\code_heo\project\OSS_Project\AI\myenv
+set VIRTUAL_ENV=C:\oss\OSS_Project\AI\myenv
 
 if not defined PROMPT set PROMPT=$P$G
 
