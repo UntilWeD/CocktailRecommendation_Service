a819bd4225b75a37126182e2abc2499005511231
Glados <muojeso90@gmail.com>
2024-11-18 13:53:52 +0900

    [ADD] cocktail 데이터 db 업로드, 연결 후 조회하여 출력 기능 추가

--- CocktailRecommendationService/run.py
+++ CocktailRecommendationService/run.py
@@ -1,9 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from app.service.service import CocktailService
+
+
+app = Flask(__name__, template_folder='app/templates')
+
+cocktail_service = CocktailService()
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    try:
+        data = cocktail_service.get_cocktails()
+
+        if data is not None:
+            return render_template('index.html', data=data)
+        else:
+            return "데이터를 가져오는데 실패하였습니다."
+
+    except Exception as e:
+        return f"에러 발생: {str(e)}"
+
 
 
 if __name__ == '__main__':
